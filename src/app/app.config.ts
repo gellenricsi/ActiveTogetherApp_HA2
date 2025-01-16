6921037de9e49cddb90ef8deb54edcfb16d1cb9d
gellenricsi <ricsi16555@gmail.com>
2025-01-16 17:20:24 +0100

    fix: fix provider alert in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { StoreService } from './shared/store.service';
 import { BackendService } from './shared/backend.service';
@@ -13,7 +12,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(),
     provideHttpClient(),
     StoreService,
     BackendService, provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync('noop'),
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { DataComponent } from './data/data.component';
 import { AddDataComponent } from './add-data/add-data.component';
 import { ButtonComponent } from './button/button.component';
 import { CommonModule } from '@angular/common';
-import { StoreService } from '../shared/store.service';
-import { BackendService } from '../shared/backend.service';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
